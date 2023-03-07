be06392f6842d541df3957ca28e03c71ad95ce25
TheTwin96 <annielvallevalera96@outlook.com>
2023-03-06 18:27:21 -0600

    Project adapted to class component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../style_sheets/Button.css';
 
-function Button({ text, isClickButton, manageClick }) {
-    return (
-        <button className={ isClickButton ? "click_button" : "restar_button" } onClick={manageClick}>{ text }</button>
-    );
+class Button extends React.Component {
+    render () {
+        return (
+            <button 
+                className={ this.props.isClickButton ? "click_button" : "restar_button" } 
+                onClick={this.props.manageClick}>{ this.props.text }
+            </button>
+        );
+    }
 }
 
 export default Button;
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../style_sheets/Counter.css';
 
-function Counter({ clickNumber }) {
-    return (
-        <div className='counter'>
-            { clickNumber }
-        </div>
-    );
+class Counter extends React.Component {
+    render () {
+        return (
+            <div className='counter'>
+                { this.props.clickNumber }
+            </div>
+        );
+    }
 }
 
 export default Counter;
